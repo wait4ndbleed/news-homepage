109fb3e062a934fb8b825fc530c73a89dc0464b9
wait4ndbleed <73363131+wait4ndbleed@users.noreply.github.com>
2023-10-01 23:22:31 -0300

    Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700', '900'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,111 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from "react"
 
 export default function Home() {
+  const [ menu, setMenu ] = useState<boolean>(false);
+
+  const handleToggle = () => {
+    if (menu) {
+      setMenu(false);
+    } else {
+      setMenu(true);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header className='my-10 container max-w-6xl m-auto'>
+        <div className="mx-4 flex justify-between">
+          <div>
+            <img src="./assets/images/logo.svg" alt="Logo" />
+          </div>
+          <div className={`${menu && 'bg-black/40 fixed'} ${!menu && 'mt-[-30px]'} md:mt-2 right-0 top-0 left-0 bottom-0 flex justify-end md:items-center`}>
+            <div className={`${menu && 'h-screen bg-white w-[80%]'} flex flex-col items-end`}>
+              <button className={`mt-10 mr-2 md:hidden ${menu && 'mr-7'}`} onClick={handleToggle}>
+                {menu && <img src="./assets/images/icon-menu-close.svg" alt="" />}
+                {!menu && <img src="./assets/images/icon-menu.svg" alt="" />}
+              </button>
+              <ul className={`${!menu && 'hidden'} md:flex gap-7 pl-4 w-full`}>
+                <li className="mb-3"><a href="#" className="hover:text-[#e9ab53]">Home</a></li>
+                <li className="mb-3"><a href="#" className="hover:text-[#e9ab53]">New</a></li>
+                <li className="mb-3"><a href="#" className="hover:text-[#e9ab53]">Popular</a></li>
+                <li className="mb-3"><a href="#" className="hover:text-[#e9ab53]">Trending</a></li>
+                <li className="mb-3"><a href="#" className="hover:text-[#e9ab53]">Categories</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </header>
+      <main className="mx-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 container max-w-6xl m-auto gap-8 mb-12">
+          <section className="md:col-span-2">
+            <div>
+              <img src="./assets/images/image-web-3-desktop.jpg" alt="" />
+            </div>
+            <div className="flex flex-col md:flex-row mt-6">
+              <div className="text-5xl mb-5 md:mb-0 font-extrabold">
+                The Bright Future of Web 3.0?
+              </div>
+              <div>
+                <p className="mb-8">
+                  We dive the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?
+                </p>
+                <button className="font-bold text-white hover:bg-[#00001a] text-sm tracking-[5px] px-8 py-4 mb-10 md:mb-0 bg-[#f15e50]">
+                  READ MORE
+                </button>
+              </div>
+            </div>
+          </section>
+          <section className="bg-[#00001a] text-white p-6">
+            <h2 className="text-4xl font-bold mb-7 text-[#e9ab53]">New</h2>
+            <div className="border-b border-[#c5c6ce]/40">
+              <a href="#" className="font-bold hover:text-[#e9ab53]">Hydrogen VS electric Cars</a>
+              <p className="text-sm mb-7 mt-3 text-[#c5c6ce]">Will hydrogen-fueled cars ever catch up to EVs?</p>
+            </div>
+            <div className="border-b border-[#c5c6ce]/40 mt-5">
+              <a href="#" className="font-bold hover:text-[#e9ab53]">The Downsides of AI Artistry</a>
+              <p className="text-sm mb-7 nt-3 text-[#c5c6ce] font-light">What are the possible adverse effects of on-demand AI image generation?</p>
+            </div>
+            <div className="mt-5">
+              <a href="#" className="font-bold mb-3 hover:text-[#e9ab53]">Is VC Funding Drying Up?</a>
+              <p className="text-sm mt-3 text-[#c5c6ce]">Private funding by VC Firms is down 50% YOY. We take a look at what that means.</p>
+            </div>
+          </section>
+          <section className="md:col-span-3 gap-5 grid grid-cols-1 md:grid-cols-3 mt-4">
+            <div className="flex gap-5">
+              <div className="w-[120px]">
+                <img src="./assets/images/image-retro-pcs.jpg" className="w-[120px]" alt="" />
+              </div>
+              <div className="flex-1">
+                <h2 className="text-[#c5c6ce] text-4xl font-bold">01</h2>
+                <a href="#" className="text-lg font-extrabold my-3 block hover:text-[#f15e50]">Reviving Retro PCs</a>
+                <p>What happens when old PCs are given modern upgrades?</p>
+              </div>
+            </div>
+            <div className="flex gap-5">
+              <div className="w-[120px]">
+                <img src="./assets/images/image-top-laptops.jpg" className="w-[120px]" alt="" />
+              </div>
+              <div className="flex-1">
+                <h2 className="text-[#c5c6ce] text-4xl font-bold">02</h2>
+                <a href="#" className="text-lg font-extrabold my-3 block hover:text-[#f15e50]">Top 10 Laptops of 2022</a>
+                <p>Our best picks for various needs and budgets.</p>
+              </div>
+            </div>
+            <div className="flex gap-5">
+              <div className="w-[120px]">
+                <img src="./assets/images/image-gaming-growth.jpg" className="w-[120px]" alt="" />
+              </div>
+              <div className="flex-1">
+                <h2 className="text-[#c5c6ce] text-4xl font-bold">03</h2>
+                <a href="#" className="text-lg font-extrabold my-3 block hover:text-[#f15e50]">The Growth of Gaming</a>
+                <p>How the pandemic has sparked fresh opprtunities.</p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
   )
 }
